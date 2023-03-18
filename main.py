30fcf5c6c870a999a1101c78361bded7c285930d
Sílvio Gouveia Romão <arquivo.romao@sapo.pt>
2023-03-18 20:43:12 +0000

    Programa final

--- main.py
+++ main.py
@@ -2,9 +2,11 @@
 print("Welcome to the tip calculator.")
 bill = float(input("What was the total bill? €"))
 tip = int(input("What percentage tip would like to give? 10,12, or 15? "))
-Num_People = int("How many people to split the bill? ")
+Num_People = int(input("How many people to split the bill? "))
 
 # Calculate the result
-result = bill*(1+tip/100)/Num_People
+result = bill * (1 + tip / 100) / Num_People
 
 # Print the value to pay
+message = f"Each person should pay: €{result:.2f}"
+print(message)
